86d0b2073288d2d01c4fe8edc85ae72cb1887b3c
llcastro <llcastro@grupomult.com.br>
2018-12-13 16:11:29 -0200

    Initial commit

---
+++ back/target/generated-sources/annotations/br/com/rumo/back/domain/TipoVagao_.java
@@ -0,0 +1,16 @@
+package br.com.rumo.back.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(TipoVagao.class)
+public abstract class TipoVagao_ {
+
+	public static volatile SingularAttribute<TipoVagao, String> codigo;
+	public static volatile SingularAttribute<TipoVagao, Long> id;
+	public static volatile SingularAttribute<TipoVagao, String> descricao;
+
+}
+
